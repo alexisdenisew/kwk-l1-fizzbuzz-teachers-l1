30666fa165209265f6505dc478e90c0cedb70e87
alexisdenisew <lexywilliams2019@gmail.com>
2018-06-20 19:32:13 +0000

    automatically backed up by learn

--- amazon_hash.rb
+++ amazon_hash.rb
@@ -1,6 +1,6 @@
-shopping_hash = {"Hair Net" => 15.98, "Tube of Rasins" => 35, "Tall Ice cold glass of help" => 200, "Bag of air" => 15.45}
+shopping_hash = {"Hair Net" => 15.98, "Tube of Rasins" => 35, "Tall Ice cold glass of help" => 200, "Bag of OxEEgen" => 15.45}
 
 shopping_hash.each do |item_name, price|
-  puts "Shelf: #{item_name}"
-  puts "price: $#{price}"
+  puts "Item: #{item_name}"
+  puts "Price: $#{price}"
 end
